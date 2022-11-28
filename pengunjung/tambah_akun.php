b8949c529318112175d46716d5baaf169e8bae69
irmeliagst <ameliaagasta2002@gmail.com>
2022-11-28 09:57:58 +0700

    first commit

---
+++ pengunjung/tambah_akun.php
@@ -0,0 +1,131 @@
+<?php 
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags --> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <title>Halaman Tambah Akun</title>
+  </head>
+  <body>
+ 
+ <!-- Form Registrasi -->
+  <div class="container">
+    <h3 class="text-center mt-3 mb-5">Talaman Tambah Akun</h3>
+    <div class="card p-5 mb-5">
+      <form method="POST" action="" enctype="multipart/form-data">
+      <div class="form-row"> 
+        <div class="form-group col-md-2">
+          <label for="id">Id</label>
+          <input type="text" class="form-control" id="id" name="id" placeholder="Masukan Id">
+        </div>
+        <div class="form-group col-md-5">
+          <label for="user">Username</label>
+          <input type="text" class="form-control" id="user" name="username" placeholder="Masukan Username">
+        </div>
+        <div class="form-group col-md-5">
+          <label for="pass">Password</label>
+          <input type="password" class="form-control" id="pass" name="password" placeholder="Masukan Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="nama">Nama Lengkap</label>
+        <input type="text" class="form-control" id="nama" name="nama_lengkap" placeholder="Masukan Nama Lengkap">
+      </div>
+      <div class="form-group">
+        <label for="jk">Jenis Kelamin</label><br>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="jenis_kelamin" id="jk" value="Laki-Laki">
+          <label class="form-check-label" for="jk">Laki-Laki</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="jenis_kelamin" id="jk" value="Perempuan">
+          <label class="form-check-label" for="jk">Perempuan</label>
+        </div>
+      </div>
+      <!-- <div class="form-group">
+        <label for="tgl">Tanggal Lahir</label>
+        <input type="date" class="form-control" id="tgl" name="tanggal_lahir">
+      </div> -->
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="alamat">Alamat</label>
+          <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukan Alamat">
+        </div>
+        <div class="form-group col-md-2">
+          <label for="telp">No. Telephone</label>
+          <input type="text" class="form-control" id="telp" name="hp" placeholder="No. Telephone">
+        </div>
+      <div class="form-group col-md-4">
+          <label for="sts">Status Registrasi</label>
+          <select id="sts" class="form-control" name="status">
+            <option selected>Pilih...</option>
+            <option value="user">User</option>
+            <option value="admin">Admin</option>
+          </select>
+        </div>
+      </div>      
+    </form>
+        <div class="form-group">
+          <label for="gambar">Foto Akun</label>
+          <input type="file" class="form-control-file border" id="gambar" name="gambar">
+        </div><br>
+        <button type="submit" class="btn btn-primary" name="tambah">Tambah</button>
+        <!-- <button type="reset" class="btn btn-danger" name="reset">Hapus</button>
+        <button type="reset" class="btn btn-danger" name="reset">Batal</button> -->
+      </form>
+      
+
+      <?php 
+  if(isset($_POST['tambah'])){
+    $id = $_POST['id'];
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+    $nama = $_POST['nama_lengkap'];
+    $jk = $_POST['jenis_kelamin'];
+    $alamat = $_POST['alamat'];
+    $telp = $_POST['telp'];
+    $sts = $_POST['status'];
+    $nama_file = $_FILES['gambar']['name'];
+    $source = $_FILES['gambar']['tmp_name'];
+    $folder = './upload/';
+
+    move_uploaded_file($source, $folder.$nama_file);
+    $insert = mysqli_query($koneksi, "INSERT INTO akun VALUES ('$id', '$nama', '$username', '$password', '$status', '$jk', '$telp', '$alamat', '$gambar')");
+
+    if($insert){
+      header("location: daftar_akun.php");
+    }
+    else {
+      echo "Maaf, terjadi kesalahan saat mencoba menyimpan data ke database";
+    }
+  }
+
+   ?>
+
+  </div>
+  </div>
+  <!-- Akhir Form Registrasi -->
+
+
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.js"></script>
+  </body>
+</html>
